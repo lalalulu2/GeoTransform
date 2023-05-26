46c9e5b4ef7efe0ea0f9fd0629e33d14c305fb8f
lalalulu2 <73332963+lalalulu2@users.noreply.github.com>
2023-05-26 21:17:56 +0300

    Update unsharp_filter.py

--- unsharp_filter.py
+++ unsharp_filter.py
@@ -9,19 +9,21 @@ directory = 'path/to/images/'
 # Получение списка путей ко всем файлам формата TIFF в папке
 file_paths = glob.glob(directory + '*.tif')
 
-# Загрузка спутниковых снимков с использованием TensorFlow-IO
+# Загрузка спутниковых снимков без использования decode_tiff
 images = []
 for file_path in file_paths:
-    image = tfio.experimental.image.decode_tiff(tf.io.read_file(file_path))
+    image = imageio.imread(file_path)
+    image = tf.convert_to_tensor(image, dtype=tf.float32) / 255.0  # Нормализация значений пикселей
     images.append(image)
 
 # Преобразование списка изображений в тензоры TensorFlow
-images = tf.convert_to_tensor(images, dtype=tf.float32)
+images = tf.stack(images)
 
 # Применение фильтра Unsharp Masking к пикселям снимков
-blurred_images = tf.image.filter2d(images, [-1, -1, -1, -1, 9, -1, -1, -1, -1], padding='SAME')
+blurred_images = tf.image.conv2d(images, tf.expand_dims(tf.constant([[0, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=tf.float32)), strides=1, padding='SAME')
 unsharp_masked_images = tf.clip_by_value(2 * images - blurred_images, 0.0, 1.0)
 
 # Сохранение результатов фильтрации в формате TIFF
 for i, file_path in enumerate(file_paths):
-    imageio.imsave('path/to/unsharp_masked_image_{}.tif'.format(i), tf.cast(unsharp_masked_images[i], tf.uint8))
+    image = tf.cast(unsharp_masked_images[i] * 255.0, tf.uint8)
+    imageio.imsave('path/to/unsharp_masked_image_{}.tif'.format(i), image)
